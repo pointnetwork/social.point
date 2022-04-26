99e609e79cd41bebf0dde83f8cd373eefd5f75ae
Antonio Jose Lopez Morillo <antoniojlopez@gmail.com>
2022-04-26 17:02:00 -0400

    Profile page validation against invalid accounts, identity tag displayed

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -6,40 +6,67 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import profilePic from '../../assets/profile-pic.jpg';
 import profileCoverImg from '../../assets/header-pic.jpg';
 import { useRoute } from "wouter";
+import { useEffect, useState } from "react";
+
+import MuiAlert from '@material-ui/lab/Alert';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const Profile = () => {
   const [match, params] = useRoute("/profile/:account");
+  const [identity, setIdentity] = useState(undefined);
+
+  useEffect(() => {
+    getIdentity();
+  }, []);
+
+  const getIdentity = async () => {
+    try {
+      const result = await window.point.identity.ownerToIdentity({owner: params.account});
+      if (result && result.data && result.data.identity) {
+        setIdentity(result.data.identity);
+      }
+    }
+    catch(error) {
+      console.log("Error");
+    }
+  };
 
   return (
-    <>
+    <> 
       <Topbar />
-      <div className="profile">
-        <div className="profileRight">
-          <div className="profileRightTop">
-            <div className="profileCover">
-              <img
-                className="profileCoverImg"
-                src={profileCoverImg}
-                alt=""
-              />
-              <img
-                className="profileUserImg"
-                src={profilePic}
-                alt=""
-              />
+      { identity ?
+        <div className="profile">
+          <div className="profileRight">
+            <div className="profileRightTop">
+              <div className="profileCover">
+                <img
+                  className="profileCoverImg"
+                  src={profileCoverImg}
+                  alt=""
+                />
+                <img
+                  className="profileUserImg"
+                  src={profilePic}
+                  alt=""
+                />
+              </div>
+              <div className="profileInfo">
+                <h4 className="profileInfoName">@{identity}</h4>
+                <span className="profileInfoDesc">{params.account}</span>
+              </div>
             </div>
-            <div className="profileInfo">
-              <h4 className="profileInfoName">{params.account}</h4>
-              <span className="profileInfoDesc">My account.</span>
+            <div className="profileRightBottom">
+              <Sidebar />
+              <Feed account={params.account} />
+              <Rightbar />
             </div>
           </div>
-          <div className="profileRightBottom">
-            <Sidebar />
-            <Feed account={params.account} />
-            <Rightbar />
-          </div>
-        </div>
-      </div>
+        </div> :
+        <Alert severity="error">Invalid account or account does not exists</Alert>
+      }
     </>
   );
 }
