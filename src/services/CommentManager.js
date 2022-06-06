c6d4dba7be1c9b65eb74e875f5d69010759534b8
Antonio José López <antoniojlopez@gmail.com>
2022-06-06 15:54:21 -0400

    Revert contract calls to make it compatible with previous PointSDK releases (<= 0.0.10)

--- src/services/CommentManager.js
+++ src/services/CommentManager.js
@@ -5,14 +5,14 @@ const EMPTY = '0x0000000000000000000000000000000000000000';
 class CommentManager {
     static getComments = async (postId) => point.contractCall("PointSocial", "getAllCommentsForPost", [postId]);
     static addComment = async (postId, storageId) =>
-        point.contractCall(
+        point.contractSend(
             "PointSocial",
             "addCommentToPost",
             [postId, storageId.startsWith('0x') ? storageId : `0x${storageId}`]
         );
-    static deleteComment = async (postId, commentId) => point.contractCall("PointSocial", "deleteCommentForPost", [postId, commentId]);
+    static deleteComment = async (postId, commentId) => point.contractSend("PointSocial", "deleteCommentForPost", [postId, commentId]);
     static editComment = async (commentId, contentId) =>
-        point.contractCall(
+        point.contractSend(
             "PointSocial",
             "editCommentForPost",
             [commentId, contentId.startsWith('0x') ? contentId : `0x${contentId}`]);
--- src/services/PostManager.js
+++ src/services/PostManager.js
@@ -5,18 +5,18 @@ const EMPTY = '0x0000000000000000000000000000000000000000';
 class PostManager {
     static getPost = async (postId) => point.contractCall("PointSocial", "getPostById", [postId]);
     static addPost = async (storageId, imageId) =>
-        point.contractCall("PointSocial", "addPost", [
+        point.contractSend("PointSocial", "addPost", [
             (storageId ? storageId.startsWith('0x') ? storageId : `0x${storageId}` : EMPTY),
             (imageId ? imageId.startsWith('0x') ? imageId : `0x${imageId}` : EMPTY)
         ]);
-    static deletePost = async (postId) => point.contractCall("PointSocial", "deletePost", [postId]);
+    static deletePost = async (postId) => point.contractSend("PointSocial", "deletePost", [postId]);
     static editPost = async (postId, contentId, imageId) =>
-        point.contractCall("PointSocial", "editPost", [
+        point.contractSend("PointSocial", "editPost", [
             postId,
             (contentId ? contentId.startsWith('0x') ? contentId : `0x${contentId}` : EMPTY)
             (imageId ? imageId.startsWith('0x') ? imageId : `0x${imageId}` : EMPTY)
         ]);
-    static addLikeToPost = async (postId) => point.contractCall("PointSocial", "addLikeToPost", [postId]);
+    static addLikeToPost = async (postId) => point.contractSend("PointSocial", "addLikeToPost", [postId]);
     static getAllPostsByOwnerLength = async (account) => point.contractCall("PointSocial", "getAllPostsByOwnerLength", [account]);
     static getAllPostsLength = async () => point.contractCall("PointSocial", "getAllPostsLength", []);
     static getPaginatedPostsByOwner = async (account, length, amount) => point.contractCall("PointSocial", "getPaginatedPostsByOwner", [account, length, amount]);
