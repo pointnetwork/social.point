b6f825ed108bc60e6993ae0574145d66b9c3da5a
Antonio José López <antoniojlopez@gmail.com>
2022-06-14 19:22:21 -0400

    Implemented deployment using docker image from Docker Hub

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -l
+
+
+mkdir -p /root/.point/keystore/
+echo "{}" > /root/.point/keystore/arweave.json
+echo "{ \"phrase\": \"$DEPLOYER\" }" > /root/.point/keystore/key.json
+
+cd /app
+
+npm start &
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:8666/status/ping); do
+    printf '.'
+    sleep 5
+done
+
+echo "Point node started!"
+
+DEPLOY_ARGS=""
+
+if [ ${DEPLOY_CONTRACTS} ]; then
+	DEPLOY_ARGS="${DEPLOY_ARGS} --contracts"	
+fi
+
+if [ "${DEPLOY_DEV}" ]; then
+	DEPLOY_ARGS="${DEPLOY_ARGS} --dev"	
+fi
+
+ls 
+
+./point deploy $GITHUB_WORKSPACE $DEPLOY_ARGS
