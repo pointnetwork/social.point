3de83d4e86c94fed279f773b24d732e1d3fac1dc
Darren Jensen <darren.jensen@gmail.com>
2022-06-16 13:41:40 +0700

    fix contract calls for latest PointSDK

--- src/services/UserManager.js
+++ src/services/UserManager.js
@@ -2,7 +2,7 @@ import point from "./PointSDK"
 
 class UserManager {
     static getProfile = async (userAddress) => point.contractCall("PointSocial", "getProfile", [userAddress]);
-    static setProfile = async (displayName, displayLocation, displayAbout, avatar, banner) => point.contractSend("PointSocial", "setProfile", [displayName, displayLocation, displayAbout, avatar, banner]);
+    static setProfile = async (displayName, displayLocation, displayAbout, avatar, banner) => point.contractCall("PointSocial", "setProfile", [displayName, displayLocation, displayAbout, avatar, banner]);
 }
 
 export default UserManager
