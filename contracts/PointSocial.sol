3e0dec1045f5b1593dc694effc9187b6945ae125
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-07-05 17:35:37 +0200

    use symbolic link

--- contracts/PointSocial.sol
+++ contracts/PointSocial.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-import "point-contract-manager/contracts/IIdentity.sol";
+import "./point-contracts/IIdentity.sol";
 
 contract PointSocial is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     using Counters for Counters.Counter;
