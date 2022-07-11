1cdf757d588056898afcad124284ec9047a04eab
Antonio José López <antoniojlopez@gmail.com>
2022-07-11 16:00:08 -0400

    Fix post update action

--- src/services/PostManager.js
+++ src/services/PostManager.js
@@ -5,18 +5,19 @@ const EMPTY = '0x0000000000000000000000000000000000000000';
 class PostManager {
     static getPost = async (postId) => point.contractCall("PointSocial", "getPostById", [postId]);
     static isFlaggedPost = async (postId) => point.contractCall("PointSocial", "postIsFlagged", [postId]);
-    static addPost = async (storageId, imageId) =>
+    static addPost = async (contentId, imageId) =>
         point.contractCall("PointSocial", "addPost", [
-            (storageId ? storageId.startsWith('0x') ? storageId : `0x${storageId}` : EMPTY),
-            (imageId ? imageId.startsWith('0x') ? imageId : `0x${imageId}` : EMPTY)
+            (contentId) ? contentId : EMPTY,
+            (imageId) ? imageId : EMPTY
         ]);
     static deletePost = async (postId) => point.contractCall("PointSocial", "deletePost", [postId]);
-    static editPost = async (postId, contentId, imageId) =>
-        point.contractCall("PointSocial", "editPost", [
+    static editPost = async (postId, contentId, imageId) => {
+        return point.contractCall("PointSocial", "editPost", [
             postId,
-            (contentId ? contentId.startsWith('0x') ? contentId : `0x${contentId}` : EMPTY)
-            (imageId ? imageId.startsWith('0x') ? imageId : `0x${imageId}` : EMPTY)
+            (contentId) ? contentId : EMPTY,
+            (imageId) ? imageId : EMPTY
         ]);
+    }
     static flagPost = async (postId) => point.contractCall("PointSocial", "flagPost", [postId]);
     static addLikeToPost = async (postId) => point.contractSend("PointSocial", "addLikeToPost", [postId]);
     static addDislikeToPost = async (postId) => point.contractSend("PointSocial", "addDislikeToPost", [postId]);
