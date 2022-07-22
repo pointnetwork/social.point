ee26087db6ac177e860cf19e688cacc4f9d64f0b
Antonio José López <antoniojlopez@gmail.com>
2022-07-22 17:01:18 -0400

    Implemented links to URLs and profiles in comments, setted the profile detection up to 14 characters

--- src/components/generic/CollapsibleTypography.jsx
+++ src/components/generic/CollapsibleTypography.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
+import processString from 'react-process-string';
 
 const useStyles = makeStyles((theme) => ({
     block: {
@@ -20,7 +21,34 @@ const useStyles = makeStyles((theme) => ({
 
 const CollapsibleTypography = ({content, loading}) => {
     const [collapsed, setCollapsed] = useState(true);
+    const [rendered, setRendered] = useState(content);
     const styles = useStyles();
+
+    useEffect(() => {
+
+        let config = [{
+            regex: /(http|https):\/\/(\S+)\.([a-z]{2,}?)(.*?)( |\,|$|\.)/gim,
+            fn: (key, result) => <span key={key}>
+                                     <a target="_blank" rel="noreferrer" href={`${result[1]}://${result[2]}.${result[3]}${result[4]}`}>{result[2]}.{result[3]}{result[4]}</a>{result[5]}
+                                 </span>
+        }, {
+            regex: /(\S+)\.([a-z]{2,}?)(.*?)( |\,|$|\.)/gim,
+            fn: (key, result) => <span key={key}>
+                                     <a target="_blank" rel="noreferrer" href={`http://${result[1]}.${result[2]}${result[3]}`}>{result[1]}.{result[2]}{result[3]}</a>{result[4]}
+                                 </span>
+        }, {
+            regex: /\@([a-z0-9_\-]{1,15}+)( |\,|$|\.)/gim, //regex to match a username
+            fn: (key, result) => {
+                let username = result[1];
+                return <span key={key}><a href={`/profile/${username}`}>@{username}</a>&nbsp;</span>
+            }
+
+        }];
+
+        setRendered(processString(config)(content));
+
+    }, []);
+
 
     const toggleCollapse = () => {
         setCollapsed(!collapsed);
@@ -30,11 +58,11 @@ const CollapsibleTypography = ({content, loading}) => {
         collapsed
         ?
         <Typography component="span" variant="body2" className={styles.inline} color="textPrimary" align="left" noWrap={true} onClick={toggleCollapse}>
-            {loading ? <Skeleton /> : content }
+            {loading ? <Skeleton /> : rendered }
         </Typography>
         : 
         <Typography component="span" variant="body2" className={styles.block} color="textPrimary" align="left" onClick={toggleCollapse}>
-            {loading ? <Skeleton /> : content }
+            {loading ? <Skeleton /> : rendered }
         </Typography>            
     );
 };
